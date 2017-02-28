fb046f0909951c5b74207551b65381fd285aaf19
drkim <drkim@ansan.ac.kr>
2017-02-28 16:02:19 +0900

    master branch change a code

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
  
-		System.out.println(" ansan ");
+		System.out.println("Ansan University ");
 
  
 		// TODO Auto-generated method stub
